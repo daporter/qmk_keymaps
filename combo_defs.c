227c535bbdf1386f4e9cb03be7785e7fb6670d32
David Porter <david@daporter.net>
2024-04-03 14:59:52 +0200

    Tweak the combos to make them more comfortable

--- combo_defs.c
+++ combo_defs.c
@@ -1,21 +1,23 @@
 #include "handsdown_au.h"
 
-const uint16_t PROGMEM Htab_combo[]   = { HD_LT4, HD_LT3, COMBO_END };
-const uint16_t PROGMEM Hstab_combo[]  = { HD_LT4, HD_LT3, HD_LT2, COMBO_END };
-const uint16_t PROGMEM H_Z_combo[]    = { HD_LT3, HD_LT1, COMBO_END };
-const uint16_t PROGMEM F_ESC_combo[]  = { HD_LM4, HD_LM1, COMBO_END };
-const uint16_t PROGMEM H_Q_combo[]    = { HD_LM3, HD_LM1, COMBO_END };
-const uint16_t PROGMEM F_CAPS_combo[] = { HD_LM3, HD_LM2, HD_RM2, HD_RM3,
-                                          COMBO_END };
-const uint16_t PROGMEM H_CAPW_combo[] = { HD_LM2, HD_LM1, HD_RM1, HD_RM2,
-                                          COMBO_END };
+// clang-format off
+const uint16_t PROGMEM H_Q_combo[]    = { HD_LT3, HD_LT2, COMBO_END };
+const uint16_t PROGMEM H_Z_combo[]    = { HD_LT2, HD_LT1, COMBO_END };
+const uint16_t PROGMEM F_ESC_combo[]  = { HD_LM3, HD_LM2, COMBO_END };
+const uint16_t PROGMEM Hstab_combo[]  = { HD_LM3, HD_LM2, HD_LM1, COMBO_END };
+const uint16_t PROGMEM Htab_combo[]   = { HD_LM2, HD_LM1, COMBO_END };
+const uint16_t PROGMEM F_CAPS_combo[] = { HD_LM2, HD_RM2, COMBO_END };
+const uint16_t PROGMEM H_CAPW_combo[] = { HD_LM1, HD_RM1, COMBO_END };
+// clang-format on
 
 combo_t key_combos[] = {
-	[HC_TAB]  = COMBO(Htab_combo, KC_TAB),
-	[HC_STAB] = COMBO(Hstab_combo, S(KC_TAB)),
-	[HC_Z]    = COMBO(H_Z_combo, HD_Z),
-	[FC_ESC]  = COMBO(F_ESC_combo, KC_ESC),
-	[HC_Q]    = COMBO(H_Q_combo, HD_Q),
+	// clang-format off
+	[HC_Q]    = COMBO(H_Q_combo,    HD_Q),
+	[HC_Z]    = COMBO(H_Z_combo,    HD_Z),
+	[FC_ESC]  = COMBO(F_ESC_combo,  KC_ESC),
+	[HC_STAB] = COMBO(Hstab_combo,  S(KC_TAB)),
+	[HC_TAB]  = COMBO(Htab_combo,   KC_TAB),
 	[FC_CAPS] = COMBO(F_CAPS_combo, KC_CAPS),
 	[HC_CAPW] = COMBO(H_CAPW_combo, CW_TOGG),
+	// clang-format on
 };
